b14e18ca47ca43c2b047c36e6a6250303f76bde5
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2025-01-12 22:10:18 +0530

    Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Spinthewheel from './Components/Spinthewheel/Spinthewheel'
 import Quiz from './Components/Quiz/Quiz'
 import Mario from './Components/Mario/Mario'
+import Home from './Components/Home/Home'
 
 
 function App() {
@@ -11,10 +12,14 @@ function App() {
     <Router>
       <div className="App">     
         <Routes>
-          <Route path="/" element={
+        <Route path="/" element={
+            <>
+              <Home />
+            </>
+          } />
+          <Route path="/SpinTheWheel" element={
             <>
               <Spinthewheel />
-            
             </>
           } />
           <Route path="/quiz" element={<Quiz />} />
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import './Home.scss'
+
+const Home = () => {
+    return(
+        <div className="Home">
+        <div className="ParentDiv">
+            <div className="child1"></div>
+            <div className="child2"></div>
+            <div className="child3"></div>
+        </div>
+        </div>
+    )
+}
+
+export default Home
