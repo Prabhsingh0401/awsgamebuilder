b2d9153ac9b1ef9202d90c52cf7741ca662696c2
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2025-01-14 23:46:02 +0530

    screenshots uploaded

--- server.js
+++ server.js
@@ -107,6 +107,5 @@ app.get('/leaderboard', async (req, res) => {
   }
 });
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
